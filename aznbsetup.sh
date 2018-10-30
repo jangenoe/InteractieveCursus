4ab1c4e5d586a8be44d809a763ff1791ce4c62e4
Jan Genoe <jan.genoe@imec.be>
2018-10-30 01:16:21 +0100

    Update

---
+++ aznbsetup.sh
@@ -0,0 +1,7 @@
+jupyter nbextension enable equation-numbering/main
+jupyter nbextension enable hide_input_all/main
+jupyter nbextension enable init_cell/main
+
+# Notebooks w/ extensions that auto-run code must be "trusted" to work the first time
+jupyter trust ToegepasteAnalogeElektronica/Klasse-C.ipynb
+jupyter trust ToegepasteAnalogeElektronica/Filters.ipynb
