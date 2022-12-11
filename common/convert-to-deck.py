51e262500121982c0c8e6a4c21441a2e8b35aecb
Jan Genoe <jan.genoe@imec.be>
2022-12-11 23:09:22 +0100

    for jupyter deck

--- common/convert-to-deck.py
+++ common/convert-to-deck.py
@@ -3,11 +3,25 @@ from glob import glob
 
 # Collect a list of all notebooks in the content folder
 notebooks = glob("ToegepasteAnalogeElektronica/*.ipynb", recursive=True)
+notebooks+ = glob("AnalogeElektronica2/*.ipynb", recursive=True)
+notebooks+ = glob("AnalogDesignTechniques/*.ipynb", recursive=True)
 
 for ipath in notebooks:
     print("file om te zetten: ",ipath)
     ntbk = nbf.read(ipath, nbf.NO_CONVERT)
-
+    
+    ntbk.cells[0].metadata.pop('slideshow')
+    ntbk.cells[0].metadata|={"@deathbeds/jupyterlab-fonts": {
+             "styles": {
+              "": {
+               "body[data-jp-deck-mode='presenting'] &": {
+                "position": "fixed",
+                "top": "-17.5%"
+               }
+              }
+             }
+        }}
+    
     u=nbf.v4.new_markdown_cell(source=["<div style = \"height: 700px; width:100%; background-color:#1D8DB0; color:#fff\"></div>"])
     u.metadata={
         "@deathbeds/jupyterlab-fonts": {
@@ -24,6 +38,7 @@ for ipath in notebooks:
              }
         },
         "slideshow": {"slide_type": "slide"},
+        "jupyterlab-deck": {"layer": "slide"},
         "tags": ["remove_cell","remove_cell4reveal"]
     }
     ntbk.cells.insert(0, u)
@@ -88,10 +103,11 @@ for ipath in notebooks:
         "tags": ["remove_cell","remove_cell4reveal"]
     }
     ntbk.cells.insert(preslides+2, u)
+    
     # voor alle code cellen: als niet gesloten --> sluiten
     
-#    for index,cell in enumerate(ntbk.cells):
-#        if "code" in cell.get('cell_type', {}):
-#            cell.metadata|={"jupyter": {"source_hidden": true}}
+    for index,cell in enumerate(ntbk.cells):
+        if "code" in cell.get('cell_type', {}):
+            cell.metadata|={"jupyter": {"source_hidden": True}}
 
     nbf.write(ntbk, ipath)
