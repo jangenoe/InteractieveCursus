d7c88cfb5b9baf826ffe0ee35691876078f0d6f5
jangenoe <jangenoe@users.noreply.github.com>
2023-11-12 22:07:32 +0000

    deploy: 4f7dacb37fb0ddb6ca3706fca9aadb7975025b3d

---
+++ Deck/AnalogeElektronica2/extensions/jupyterlab-myst/static/react-syntax-highlighter_languages_highlight_mojolicious.b47d952c0ed6b84bda37.js
@@ -0,0 +1,1 @@
+(self.webpackChunkjupyterlab_myst=self.webpackChunkjupyterlab_myst||[]).push([[2134],{92210:e=>{e.exports=function(e){return{name:"Mojolicious",subLanguage:"xml",contains:[{className:"meta",begin:"^__(END|DATA)__$"},{begin:"^\\s*%{1,2}={0,2}",end:"$",subLanguage:"perl"},{begin:"<%{1,2}={0,2}",end:"={0,1}%>",subLanguage:"perl",excludeBegin:!0,excludeEnd:!0}]}}}}]);
