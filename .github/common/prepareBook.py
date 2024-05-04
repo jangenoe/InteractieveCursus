4653a30cb7928a2f70c7d15162c20143898f7442
Jan Genoe <jan.genoe@gmail.com>
2024-05-04 23:58:44 +0200

    adding scaling details

---
+++ .github/common/prepareBook.py
@@ -0,0 +1,15 @@
+import nbformat as nbf
+from glob import glob
+
+# Collect a list of all notebooks in the content folder
+notebooks = glob("ToegepasteAnalogeElektronica/*.ipynb", recursive=True)
+notebooks+= glob("AnalogeElektronica2/*.ipynb", recursive=True)
+notebooks+= glob("AnalogDesignTechniques/*.ipynb", recursive=True)
+notebooks+= glob("MicroEnNanoTechnologie/*.ipynb", recursive=True)
+
+for ipath in notebooks:
+    ntbk = nbf.read(ipath, nbf.NO_CONVERT)
+    for index,cell in enumerate(ntbk.cells):
+        if "remove-cell4BOOK" in cell.metadata.get('tags', []):
+            cell.metadata.tags+=["remove-cell"]
+    nbf.write(ntbk, ipath)
