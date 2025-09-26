0cb283db1e53af366ca5554e024d9a2b08e163ef
Jan Genoe <jan.genoe@gmail.com>
2025-09-27 01:04:30 +0200

    Update convert-to-pptx.py

--- .github/common/convert-to-pptx.py
+++ .github/common/convert-to-pptx.py
@@ -11,6 +11,9 @@ from pptx.enum.shapes import MSO_AUTO_SHAPE_TYPE, PP_PLACEHOLDER, MSO_SHAPE_TYPE
 from PIL import UnidentifiedImageError
 from IPython.lib.latextools import latex_to_png
 
+from bs4 import BeautifulSoup
+import re
+
 # Collect a list of all notebooks in the content folder
 notebooks = glob("ToegepasteAnalogeElektronica/*.ipynb", recursive=True)
 notebooks+= glob("AnalogeElektronica2/*.ipynb", recursive=True)
@@ -176,6 +179,25 @@ for ipath in notebooks:
                                     pictp.left=(prs.slide_width-pictp.width)//2
                             except UnidentifiedImageError:
                                 print("  image/jpeg  error for cell number "+str(index))
+                        elif "text/html" in output.get("data", {}):
+                            lines="".join(output.data["text/html"]).split('\n')
+                            for i in range(0,len(lines),lines_per_chunk):
+                                slide = prs.slides.add_slide(prs.slide_layouts[KUL_layout_code])
+                                maketitle(cell,slide) 
+                                soup = BeautifulSoup("\n".join(lines[i:i+lines_per_chunk]), "html.parser")
+                                p=slide.shapes[0].text_frame.paragraphs[0]
+                                for span in soup.find_all("span"):
+                                    style = span.get("style", "")
+                                    color_match = re.search(r"color:\s*(#[0-9a-fA-F]{6})", style)
+                                    color = RGBColor(0, 0, 0)  # default black
+                                    if color_match:
+                                        hex_color = color_match.group(1)
+                                        color = RGBColor(*(int(hex_color[i:i+2], 16) for i in (1, 3, 5)))
+                                    run = p.add_run()
+                                    run.text = span.text
+                                    run.font.color.rgb = color
+                                    run.font.name = 'Courier New'
+                                    run.font.size = Pt(14)
                         elif "text/plain" in output.get("data", {}):
                             lines="".join(output.data["text/plain"]).split('\n')
                             for i in range(0,len(lines),lines_per_chunk):
